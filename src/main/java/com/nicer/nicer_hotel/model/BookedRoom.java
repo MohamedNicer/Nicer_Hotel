29f2edb112ae3595007fdf4ff1a1a5ba3539e7b8
MohamedNicer <mohamednicer.mn@gmail.com>
2023-12-12 07:33:23 +0000

    Initial Commit

--- src/main/java/com/nicer/nicer_hotel/model/BookedRoom.java
+++ src/main/java/com/nicer/nicer_hotel/model/BookedRoom.java
@@ -1,10 +1,62 @@
 package com.nicer.nicer_hotel.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
 /**
  * Author: Mohamed Nicer
  * User:mohamednicer
  * Date:12/12/2023
  * Time:05:12
  */
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long bookingId;
+
+    @Column(name = "check_in")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_fullname")
+    private String guestFullName;
+
+    @Column(name = "guest_email")
+    private String guestEmail;
+
+    @Column(name = "adults")
+    private int numOfAdults;
+
+    @Column(name = "children")
+    private int numOfChildren;
+
+    @Column(name = "total_guests")
+    private int totalNumOfGuest;
+
+    @Column(name = "confirmation_code")
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumberOfGuests(){
+        this.totalNumOfGuest = this.numOfAdults + this.numOfChildren;
+    }
+    public void setNumOfAdults(int numOfAdults) {
+        this.numOfAdults = numOfAdults;
+        calculateTotalNumberOfGuests();
+    }
+    public void setNumOfChildren(int numOfChildren) {
+        this.numOfChildren = numOfChildren;
+        calculateTotalNumberOfGuests();
+    }
 }
