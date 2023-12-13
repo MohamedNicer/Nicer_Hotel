5d739d7d8e160e99db27862b9d5ffc7e119d06bb
MohamedNicer <mohamednicer.mn@gmail.com>
2023-12-13 16:09:46 +0000

    Add New Room + RoomType + Cors enabled

---
+++ client/nicer-hotel/src/components/room/AddRoom.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+
+const AddRoom = () => {
+    const[newRoom , setNewRoom] = useState({
+        roomImage : null,
+        roomType : "",
+        roomPrice : ""
+    })
+
+    const[imagePreview ,setImagePreview] = useState("")
+    const[successMessage, setSuccessMessage] = useState("")
+    const[errorMessage, setErrorMessage] = useState("")
+
+    const handleRoomInputChange = (e) =>{
+        const name = e.target.name
+        let value = e.target.value
+        if(name=="roomPrice"){
+            if(!isNaN(value)){
+                value.parseInt(value)
+            }else{
+                value = ""
+            }
+        }
+        setNewRoom({...newRoom, [name]: value})
+    }
+  return (
+    <div></div>
+  )
+}
+
+export default AddRoom
---
+++ client/nicer-hotel/src/components/utils/ApiFunctions.js
@@ -0,0 +1,30 @@
+import axios from "axios"
+
+export const api = axios.create({
+    baseURL: "http://localhost:8080"
+})
+
+/* This function is used to add new room to db */
+export async function addRoom(roomImage, roomType, roomPrice){
+    const formData = new FormData();
+    formData.append("roomImage",roomImage);
+    formData.append("roomType",roomType);
+    formData.append("roomPrice",roomPrice);
+
+    const response = await api.post("/rooms/addRoom", formData)
+    if(response.status == 201){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+/* This function is used to fetch all room types from db */
+export async function getRoomTypes(){
+    try {
+        const response = await api.get("/rooms/roomTypes")
+        return response.data
+    } catch (error) {
+        throw new Error("Error Fetching Room Types")
+    }
+}
